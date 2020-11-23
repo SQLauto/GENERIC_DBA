71f5b85065df79d20bbc6dea6584fd6dd206ae17
Dave Babler <dbabler@mezrahconsulting.com>
2020-11-23 17:13:25 -0500

    DD_Column & DD_TableExistwork

--- MS_SQLSERVER/UtilityDB_Updated/DD_EaseOfAccessSpecific/DOES IT EXIST/UTL.DD_ColumnExist.sql
+++ MS_SQLSERVER/UtilityDB_Updated/DD_EaseOfAccessSpecific/DOES IT EXIST/UTL.DD_ColumnExist.sql
@@ -2,17 +2,17 @@
 -- Author:		    Dave Babler
 -- Create date:     08/25/2020
 -- Last Modified:   11/23/2020
--- Description:	    Checks to see if table exists use output boolean for logic flow in other procedures
+-- Description:	    Checks to see if column in table exists 
+--                  use output boolean for logic flow in other procedures
 -- =============================================
 CREATE
 	OR
 
-ALTER PROCEDURE UTL.DD_TableExist @ustrTableName NVARCHAR(64)
-	, @ustrDBName NVARCHAR(64)
-	, --SHOULD BE PASSED IN FROM ANOTHER PROC
-	@ustrSchemaName NVARCHAR(64)
-	, --SHOULD BE PASSED IN FROM ANOTHER PROC
-	@boolSuccessFlag BIT OUTPUT
+ALTER PROCEDURE UTL.DD_ColumnExist @ustrTableName NVARCHAR(64)
+	, @ustrColumnName NVARCHAR(64)
+	, @ustrDBName NVARCHAR(64) --SHOULD BE PASSED IN FROM ANOTHER PROC
+	, @ustrSchemaName NVARCHAR(64) --SHOULD BE PASSED IN FROM ANOTHER PROC
+	, @boolSuccessFlag BIT OUTPUT
 	, @ustrMessageOut NVARCHAR(400) = NULL OUTPUT
 AS
 SET NOCOUNT ON;
@@ -21,30 +21,34 @@ BEGIN TRY
 	/** If the table doesn't exist we're going to output a message and throw a false flag,
      *  ELSE we'll throw a true flag so external operations can commence
      * Dave Babler 2020-08-26  */
-	DECLARE @ustrOutGoingMessageEnd NVARCHAR(48) = N' does not exist, check spelling, try again?';
 	DECLARE @ustrQuotedDB NVARCHAR(128) = N'' + QUOTENAME(@ustrDBName) + '';
 	DECLARE @intRowCount INT;
 	DECLARE @SQLCheckForTable NVARCHAR(1000) = 'SELECT 1 
                                FROM ' + @ustrQuotedDB + 
-		'.INFORMATION_SCHEMA.TABLES 
+		'.INFORMATION_SCHEMA.COLUMNS 
                                WHERE TABLE_NAME = @ustrTable 
-                                    AND TABLE_SCHEMA = @ustrSchema'
+                                    AND TABLE_SCHEMA = @ustrSchema
+                                    	AND COLUMN_NAME = @ustrColumn'
 		;
 
 
 
+
 	EXECUTE sp_executesql @SQLCheckForTable
-		, N'@ustrTable NVARCHAR(64), @ustrSchema NVARCHAR(64)'
+		, N'@ustrTable NVARCHAR(64), 
+            @ustrSchema NVARCHAR(64),
+            @ustrColumn NVARCHAR(64)'
 		, @ustrTable = @ustrTableName
-		, @ustrSchema = @ustrSchemaName;
+		, @ustrSchema = @ustrSchemaName
+        , @ustrColumn = @ustrColumnName;
 
 
 
 
-	IF @intRowCount <> 1
+    IF @intRowCount <> 1
 	BEGIN
 		SET @boolSuccessFlag = 0;
-		SET @ustrMessageOut = @ustrTableName + @ustrOutGoingMessageEnd;
+		SET @ustrMessageOut = @ustrColumnName + ' of ' + @ustrTableName + ' does not exist, check spelling, try again?';
 	END
 	ELSE
 	BEGIN
