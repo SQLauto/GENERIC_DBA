ab455aa97fc11f4af4e2427d605c541b91a60eb7
Dave Babler <dbabler@mezrahconsulting.com>
2021-02-01 10:24:41 -0500

    add function to check if it's a view fix add table comment prior to stripping out selects and prints

--- MS_SQLSERVER/UtilityDB_Updated/DD_EaseOfAccessSpecific/ADD COMMENTS/UTL.DD_AddTableComment.sql
+++ MS_SQLSERVER/UtilityDB_Updated/DD_EaseOfAccessSpecific/ADD COMMENTS/UTL.DD_AddTableComment.sql
@@ -23,13 +23,15 @@ DECLARE @vrtComment SQL_VARIANT
 	, @intRowCount INT
 	, @boolExistFlag BIT
 	, @ustrMessageOut NVARCHAR(400)
+	, @bitIsThisAView BIT
+	, @ustrViewOrTable NVARCHAR(8)
 	;
-DECLARE @ustrVariantConv NVARCHAR(MAX) = CAST(@vrtComment AS NVARCHAR(MAX)); -- leaving this conversion instead of just declaring as nvarchar. Technically it IS stored as variant, people should be aware of this.
 --still consider removing this conversion from production and just leaving good code comments.
 
 DECLARE @boolCatchFlag BIT = 0;  -- for catching and throwing a specific error. 
-
+	--set and internally cast the VARIANT, I know it's dumb, but it's what we have to do.
 SET @vrtComment = CAST(@strComment AS SQL_VARIANT);   --have to convert this to variant type as that's what the built in sp asks for.
+DECLARE @ustrVariantConv NVARCHAR(MAX) = CAST(@vrtComment AS NVARCHAR(MAX)); -- leaving this conversion instead of just declaring as nvarchar. Technically it IS stored as variant, people should be aware of this.
 
 
 
@@ -41,6 +43,11 @@ BEGIN TRY
 												, @ustrSchemaName OUTPUT
 												, @ustrObjectName OUTPUT;
 
+PRINT 'BELOW SHOULD BE databaseschemaobject';
+
+PRINT  @ustrDatabaseName + @ustrSchemaName + @ustrObjectName;
+
+
 			/**Check to see if the column or table actually exists -- Babler*/
 		
 		EXEC Utility.UTL.DD_TableExist @ustrObjectName
@@ -51,24 +58,22 @@ BEGIN TRY
 			print 'bool exist flag is below';
 			print @boolExistFlag;
 	
-	
+
+		/** Next Check to see if the name is for a view instead of a table, alter the function to fit your agency's naming conventions
+		 * Not necissary to check this beforehand as the previous calls will work for views and tables due to how
+		 * INFORMATION_SCHEMA is set up.  Unfortunately from this point on we'll be playing with Microsoft's sys tables
+		  */
+		SET @bitIsThisAView = Utility.UTL.fn_IsThisTheNameOfAView(@ustrObjectName);
+
+		IF @bitIsThisAView = 0
+			SET @ustrViewOrTable = 'TABLE';
+		ELSE
+			SET @ustrViewOrTable = 'VIEW';
+
 	IF @boolExistFlag = 0
 	BEGIN
-		--if it does not exist raise error and send to the exception tank
+
 		SET @boolCatchFlag = 1;
-/* 		SET @strErrorMessage = CONCAT('Attempt to add comment on table '
-										, @ustrDatabaseName
-										, '.'
-										, @ustrSchemaName
-										, '.'
-										, @ustrObjectName
-										, ';however  at least one part of the fully qualified name '
-										, @ustrDatabaseName
-										, '.'
-										, @ustrSchemaName
-										, '.'
-										, @ustrObjectName
-										, ' does not exist, check spelling, try again?'); */
 
 		PRINT @strErrorMessage;
 
@@ -85,6 +90,7 @@ ELSE
                         * Normally it's just a simple matter of ALTER TABLE/ALTER COLUMN ADD COMMENT, literally every other system
                         * however, Microsoft Has decided to use this sort of registry style of documentation 
                         * -- Dave Babler 2020-08-26*/
+
 		SET @intRowCount = NULL;
 		--future DBA's reading this...I can already hear your wailing and gnashing of teeth about SQL Injection. Stow it, only DBA's and devs will use this, it won't be customer facing.
 		SET @dSQLNotExistCheckProperties = N' SELECT NULL
@@ -93,6 +99,9 @@ ELSE
 											  + '.sys.extended_properties'
 											  + ' WHERE [major_id] = OBJECT_ID('
 											  + ''''
+											  + @ustrDatabaseName
+
+											  + '.'
 											  + @ustrSchemaName
 
 											  + '.'
@@ -102,17 +111,19 @@ ELSE
 											  + ')'
 											  +	' AND [name] = N''MS_Description''
 					AND [minor_id] = 0';
+		PRINT 'Existance check SQL below';
 		PRINT @dSQLNotExistCheckProperties;
 
 		EXEC sp_executesql @dSQLNotExistCheckProperties;
 
 		SET @intRowCount = @@ROWCOUNT;
-
+		PRINT 'Rowcount Below';
 		PRINT @intRowCount;
 
 		/* do an if rowcount = 0 next */
 		IF @intRowCount = 0 
 			BEGIN
+				PRINT 'IN IF STATEMENT!';
 				SET @dSQLApplyComment = N'EXEC ' 
 										+ @ustrDatabaseName 
 										+ '.'
@@ -127,84 +138,105 @@ ELSE
 										+ ''''
 										+ 	@ustrSchemaName
 										+ ''''
-										+ ', @level1type = N''TABLE'' '
+										+ ', @level1type = N'
+										+ ''''
+										+ 	@ustrViewOrTable
+										+ ''''										
 										+ ', @level1name = '
 										+ ''''
 										+	@ustrObjectName
 										+ '''';
-				PRINT @dSQLApplyComment;
-				EXEC sp_executesql @dSQLApplyComment
-	
+
+
+			PRINT CAST(@dSQLApplyComment AS VARCHAR(MAX));
 			END
 		ELSE
 			BEGIN 
 				--DYNAMIC SQL FOR UPDATE EXTENDED PROPERTY GOES HERE.
+								SET @dSQLApplyComment = N'EXEC ' 
+										+ @ustrDatabaseName 
+										+ '.'
+										+ 'sys.sp_updateextendedproperty  '
+										+ '@name = N''MS_Description'' '
+										+ ', @value = '
+										+ ''''
+										+  @ustrVariantConv
+										+ ''''
+										+ ', @level0type = N''SCHEMA'' '
+										+ ', @level0name = '
+										+ ''''
+										+ 	@ustrSchemaName
+										+ ''''
+										+ ', @level1type = N'
+										+ ''''
+										+ 	@ustrViewOrTable
+										+ ''''										
+										+ ', @level1name = '
+										+ ''''
+										+	@ustrObjectName
+										+ '''';
+
 			END
+				PRINT 'Apply sql comment is below';
+				PRINT @dSQLApplyComment;
+				EXEC sp_executesql @dSQLApplyComment
 
-			---WARNING will need to add a 'viw' looker to see if we need to do a view instead of a table
-	-- 	IF NOT EXISTS (
-	-- 			SELECT NULL
-	-- 			FROM SYS.EXTENDED_PROPERTIES
-	-- 			WHERE [major_id] = OBJECT_ID(@ustrObjectName)
-	-- 				AND [name] = N'MS_Description'
-	-- 				AND [minor_id] = 0
-	-- 			)
-    --             /** Need to execute dynamically see here
-    --              https://stackoverflow.com/questions/20757804/execute-stored-procedure-from-stored-procedure-w-dynamic-sql-capturing-output 
-    --               also will need to call it as [DBNAME].sys.sp_addextendedproperty*/
 
-	-- 		EXECUTE sp_addextendedproperty @name = N'MS_Description'
-	-- 			, @value = @vrtComment
-	-- 			, @level0type = N'SCHEMA'
-	-- 			, @level0name = @ustrSchemaName
-	-- 			, @level1type = N'TABLE'
-	-- 			, @level1name = @strTableName;
-	-- 	ELSE
-	-- 		EXECUTE sp_updateextendedproperty @name = N'MS_Description'
-	-- 			, @value = @vrtComment
-	-- 			, @level0type = N'SCHEMA'
-	-- 			, @level0name = N'dbo'
-	-- 			, @level1type = N'TABLE'
-	-- 			, @level1name = @strTableName;
-	-- END
-
-	
 	SET NOCOUNT OFF
 END TRY
 
 BEGIN CATCH
 	IF @boolCatchFlag = 1
 	BEGIN
-		INSERT INTO  Utility.UTL.DB_EXCEPTION_TANK (
-			UserName
-			, ErrorState
-			, ErrorSeverity
-			, ErrorProcedure
-			, ErrorMessage
-			, ErrorDateTime
-			)
-		VALUES (
-			SUSER_SNAME()
-			, ERROR_STATE()
-			, ERROR_SEVERITY()
-			, ERROR_PROCEDURE()
-			, ERROR_MESSAGE()
-			, GETDATE()
-			);
+
+	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK (
+		[DatabaseName]
+		, [UserName]
+		, [ErrorNumber]
+		, [ErrorState]
+		, [ErrorSeverity]
+		, [ErrorLine]
+		, [ErrorProcedure]
+		, [ErrorMessage]
+		, [ErrorDateTime]
+		)
+	VALUES (
+		DB_NAME()
+		, SUSER_SNAME()
+		, ERROR_NUMBER()
+		, ERROR_STATE()
+		, ERROR_SEVERITY()
+		, ERROR_LINE()
+		, ERROR_PROCEDURE()
+		, ERROR_MESSAGE()
+		, GETDATE()
+		);
 	END
 	ELSE
 	BEGIN
-		INSERT INTO Utility.UTL.DB_EXCEPTION_TANK
-		VALUES (
-			SUSER_SNAME()
-			, ERROR_NUMBER()
-			, ERROR_STATE()
-			, ERROR_SEVERITY()
-			, ERROR_PROCEDURE()
-			, ERROR_LINE()
-			, ERROR_MESSAGE()
-			, GETDATE()
-			);
+
+	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK (
+		[DatabaseName]
+		, [UserName]
+		, [ErrorNumber]
+		, [ErrorState]
+		, [ErrorSeverity]
+		, [ErrorLine]
+		, [ErrorProcedure]
+		, [ErrorMessage]
+		, [ErrorDateTime]
+		)
+	VALUES (
+		DB_NAME()
+		, SUSER_SNAME()
+		, ERROR_NUMBER()
+		, ERROR_STATE()
+		, ERROR_SEVERITY()
+		, ERROR_LINE()
+		, ERROR_PROCEDURE()
+		, ERROR_MESSAGE()
+		, GETDATE()
+		);
 	END
 
 	PRINT 
@@ -217,9 +249,10 @@ BEGIN CATCH
 		WITH mxe
 		AS (
 			SELECT MAX(ErrorID) AS MaxError
-			FROM Utility.UTL.DB_EXCEPTION_TANK
+			FROM CustomLog.ERR.DB_EXCEPTION_TANK
 			)
 		SELECT ErrorID
+			, DatabaseName
 			, UserName
 			, ErrorNumber
 			, ErrorState
@@ -227,7 +260,7 @@ BEGIN CATCH
 			, ErrorProcedure
 			, ErrorMessage
 			, ErrorDateTime
-		FROM Utility.UTL.DB_EXCEPTION_TANK et
+		FROM CustomLog.ERR.DB_EXCEPTION_TANK et
 		INNER JOIN mxe
 			ON et.ErrorID = mxe.MaxError
 
@@ -235,16 +268,35 @@ BEGIN CATCH
 
 '
 END CATCH;
-/*Dynamic Queries
-	-- if not exists
+	/*Dynamic Queries
+		-- properties if not exists
 
-	-- properties if not exists
+					SELECT NULL
+					FROM QUOTENAME(@ustrDatabaseName).SYS.EXTENDED_PROPERTIES
+					WHERE [major_id] = OBJECT_ID(@ustrObjectName)
+						AND [name] = N'MS_Description'
+						AND [minor_id] = 0
 
-				SELECT NULL
-				FROM QUOTENAME(@ustrDatabaseName).SYS.EXTENDED_PROPERTIES
-				WHERE [major_id] = OBJECT_ID(@ustrObjectName)
-					AND [name] = N'MS_Description'
-					AND [minor_id] = 0
+        -- add the properties  if they don't exist
+                --be advised trying to run this without dynamic sql call will not work.
+            
+		      EXECUTE @ustrDatabaseName.sp_addextendedproperty @name = N'MS_Description'
+		          , @value = @vrtComment
+		          , @level0type = N'SCHEMA'
+		          , @level0name = @ustrSchemaName
+		          , @level1type = N'TABLE'
+		          , @level1name = @strTableName;
 
+        -- replace the properties  if they already exist
+                --be advised trying to run this without dynamic sql call will not work.
+              EXECUTE @ustrDatabaseName.sp_updateextendedproperty @name = N'MS_Description'
+		          , @value = @vrtComment
+		          , @level0type = N'SCHEMA'
+		          , @level0name = N'dbo'
+		          , @level1type = N'TABLE'
+		          , @level1name = @strTableName;
+            
+		*/
 
-	*/
+	--Bibliography
+	--   https://stackoverflow.com/questions/20757804/execute-stored-procedure-from-stored-procedure-w-dynamic-sql-capturing-output 
