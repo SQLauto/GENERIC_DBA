e8df29827e1f2db4349554ac07e5d1035dd5cc14
Dave Babler <dbabler@mezrahconsulting.com>
2021-05-05 17:45:49 -0400

    delete a user

---
+++ MS_SQLSERVER/Database_System/Security_Users/DELETEAUSER.sql
@@ -0,0 +1,19 @@
+USE [master]
+GO
+DROP LOGIN yourloginhere
+GO
+
+EXECUTE master.sys.sp_MSforeachdb 'USE [?]; 
+    DECLARE @Tsql NVARCHAR(MAX)
+    SET @Tsql = ''''
+
+    SELECT @Tsql = ''DROP USER '' + d.name
+    FROM sys.database_principals d
+    JOIN master.sys.server_principals s
+        ON s.sid = d.sid
+    WHERE s.name = ''yourloginhere''
+ 
+    EXEC (@Tsql)
+
+'
+GO
