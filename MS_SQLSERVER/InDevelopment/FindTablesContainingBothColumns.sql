0e7b433fb3ae64bfbd132887fa4e3c6d4fb48221
Dave Babler <dbabler@mezrahconsulting.com>
2022-05-28 13:55:14 -0400

    formatting change

--- MS_SQLSERVER/InDevelopment/FindTablesContainingBothColumns.sql
+++ MS_SQLSERVER/InDevelopment/FindTablesContainingBothColumns.sql
@@ -1,38 +1,34 @@
 ;WITH pe
-AS (
-	SELECT TABLE_NAME
-		, COLUMN_NAME
-		, CASE 
-			WHEN DATA_TYPE = 'nvarchar'
-				THEN 'varchar'
-			ELSE DATA_TYPE
-			END AS [DATA_TYPE]
-	FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE lower(COLUMN_NAME) LIKE '%pathelect%'
-		OR lower(COLUMN_NAME) LIKE '%path_elect%'
-	)
-	, emppl
-AS (
-	SELECT TABLE_NAME
-		, COLUMN_NAME
-		, CASE 
-			WHEN DATA_TYPE = 'nvarchar'
-				THEN 'varchar'
-			ELSE DATA_TYPE
-			END AS [DATA_TYPE]
-	FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE lower(COLUMN_NAME) LIKE '%employeep%'
-		OR lower(COLUMN_NAME) LIKE '%employee_p%'
-	)
-SELECT COUNT(*)
-	, pe.TABLE_NAME
-	, pe.COLUMN_NAME
-	, emppl.COLUMN_NAME
-FROM pe
-INNER JOIN emppl
-	ON pe.TABLE_NAME = emppl.TABLE_NAME
-		AND pe.DATA_TYPE = emppl.DATA_TYPE
-GROUP BY GROUPING SETS((pe.TABLE_NAME
-                        , pe.COLUMN_NAME
-                        , emppl.COLUMN_NAME));
+   AS (   SELECT    TABLE_NAME
+                  , COLUMN_NAME
+                  , CASE
+                         WHEN DATA_TYPE = 'nvarchar' THEN
+                             'varchar'
+                         ELSE
+                             DATA_TYPE
+                    END AS "DATA_TYPE"
+          FROM  INFORMATION_SCHEMA.COLUMNS
+          WHERE LOWER(COLUMN_NAME) LIKE '%pathelect%'
+                OR  LOWER(COLUMN_NAME) LIKE '%path_elect%')
+    , emppl
+   AS (   SELECT    TABLE_NAME
+                  , COLUMN_NAME
+                  , CASE
+                         WHEN DATA_TYPE = 'nvarchar' THEN
+                             'varchar'
+                         ELSE
+                             DATA_TYPE
+                    END AS "DATA_TYPE"
+          FROM  INFORMATION_SCHEMA.COLUMNS
+          WHERE LOWER(COLUMN_NAME) LIKE '%employeep%'
+                OR  LOWER(COLUMN_NAME) LIKE '%employee_p%')
+SELECT  COUNT(*)
+      , pe.TABLE_NAME
+      , pe.COLUMN_NAME
+      , emppl.COLUMN_NAME
+FROM    pe
+    INNER JOIN emppl
+        ON pe.TABLE_NAME     = emppl.TABLE_NAME
+           AND  pe.DATA_TYPE = emppl.DATA_TYPE
+GROUP BY GROUPING SETS((pe.TABLE_NAME, pe.COLUMN_NAME, emppl.COLUMN_NAME));
 --THE TWO PARENTHESIS CREATS A GROUPED SET OF A GROUPED SET.  SO NO NULLS
